e99acec5f70a1e06ecca18372669311b01335cf5
adewunmi97 <shonibareadewunmicomfort@gmail.com>
2025-08-04 16:20:32 +0100

    deploy to fly.io

--- app/javascript/paypal_button.js
+++ app/javascript/paypal_button.js
@@ -1,8 +1,9 @@
 document.addEventListener("turbo:load", function () {
   const container = document.getElementById("paypal-button-container");
-
+  
   if (container && window.paypal) {
     const price = container.dataset.price;
+    console.log("PayPal price from dataset:", price);
 
     // Prevent multiple renders
     if (container.hasChildNodes()) return;
