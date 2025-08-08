1abe8df3c74bfd8a8d80de6845fb5b2ec12f87fc
adewunmi97 <shonibareadewunmicomfort@gmail.com>
2025-08-08 15:36:23 +0100

    add product to cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,50 +1,13 @@
 class CartItemsController < ApplicationController
-  before_action :set_cart_item, only: %i[ show edit update destroy ]
+  before_action :set_cart_item, only: %i[ destroy ]
+  before_action :authenticate_user!
 
-  # GET /cart_items or /cart_items.json
-  def index
-    @cart_items = CartItem.all
-  end
-
-  # GET /cart_items/1 or /cart_items/1.json
-  def show
-  end
-
-  # GET /cart_items/new
-  def new
-    @cart_item = CartItem.new
-  end
-
-  # GET /cart_items/1/edit
-  def edit
-  end
-
+  
   # POST /cart_items or /cart_items.json
   def create
-    @cart_item = CartItem.new(cart_item_params)
-
-    respond_to do |format|
-      if @cart_item.save
-        format.html { redirect_to @cart_item, notice: "Cart item was successfully created." }
-        format.json { render :show, status: :created, location: @cart_item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @cart_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /cart_items/1 or /cart_items/1.json
-  def update
-    respond_to do |format|
-      if @cart_item.update(cart_item_params)
-        format.html { redirect_to @cart_item, notice: "Cart item was successfully updated." }
-        format.json { render :show, status: :ok, location: @cart_item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @cart_item.errors, status: :unprocessable_entity }
-      end
-    end
+    @cart = Cart.find_or_create_by(user_id: current_user.id)
+    @product = Product.find(params[:product_id])
+    @cart.cart_items.create!(product:@product) if @cart.products.exclude?(@product)
   end
 
   # DELETE /cart_items/1 or /cart_items/1.json
