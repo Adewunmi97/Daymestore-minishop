c73d68fcf6edfd695cdc5c93680a7c9867cd11ac
adewunmi97 <shonibareadewunmicomfort@gmail.com>
2025-08-29 11:45:27 +0100

    add unt test for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+  end
+
+  test "should check if user has purchased a product" do
+    product = products(:one)
+    order = @user.orders.create!(
+      status: :fulfilled,
+      total_amount: 10
+    )
+    order.purchases.create!(
+      product: product,
+      buyer: @user
+    )
+
+    assert @user.has_purchased?(product)
+  end
 end
